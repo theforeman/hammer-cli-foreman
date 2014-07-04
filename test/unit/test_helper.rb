8d2529ad9183b023b1a123f0643d0278405b50c0
Tomas Strachota <tstrachota@redhat.com>
2014-07-04 09:59:57 +0000

    Refs #2922 - tests updated to use apidoc export for v1.6

--- test/unit/test_helper.rb
+++ test/unit/test_helper.rb
@@ -19,7 +19,7 @@ require 'hammer_cli'
 require 'hammer_cli_foreman/commands'
 
 HammerCLIForeman.stubs(:resource_config).returns({
-  :apidoc_cache_dir => 'test/unit/data/' + (ENV['TEST_API_VERSION'] || '1.5'),
+  :apidoc_cache_dir => 'test/unit/data/' + (ENV['TEST_API_VERSION'] || '1.6'),
   :apidoc_cache_name => 'foreman_api',
   :dry_run => true})
 
