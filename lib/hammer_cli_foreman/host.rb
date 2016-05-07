8d36bf1b46049a5f230addd983c2f8d5905bae83
Ranjan Kumar <ranjankumar188@gmail.com>
2016-05-08 00:20:06 +0530

    Fixes #14510 - The resources /reports were moved to /config_reports

--- lib/hammer_cli_foreman/host.rb
+++ lib/hammer_cli_foreman/host.rb
@@ -350,7 +350,7 @@ module HammerCLIForeman
 
     class ReportsCommand < HammerCLIForeman::AssociatedResourceListCommand
       command_name "reports"
-      resource :reports, :index
+      resource :config_reports, :index
       parent_resource :hosts
 
       output HammerCLIForeman::Report::ListCommand.output_definition
