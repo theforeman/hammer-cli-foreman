27f8cca50396c74466ae77e88a5ae8104e40009c
mbacovsky <martin.bacovsky@gmail.com>
2016-08-17 11:30:32 +0200

    Refs #12473 - show admin flag in user-group listing

--- test/unit/usergroup_test.rb
+++ test/unit/usergroup_test.rb
@@ -20,6 +20,7 @@ describe HammerCLIForeman::Usergroup do
     context "output" do
       it_should_print_column "Id"
       it_should_print_column "Name"
+      it_should_print_column "Admin"
     end
 
   end
@@ -37,6 +38,7 @@ describe HammerCLIForeman::Usergroup do
       with_params ["--id=1"] do
         it_should_print_column "Id"
         it_should_print_column "Name"
+        it_should_print_column "Admin"
         it_should_print_column "Users"
         it_should_print_column "User groups"
         it_should_print_column "Roles"
