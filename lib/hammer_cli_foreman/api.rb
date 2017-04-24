c84873229876ea55085c23346f47702abdd2192d
Tomas Strachota <tstrachota@redhat.com>
2017-04-24 11:42:53 +0200

    Fixes #19431 - retry command on session expiry

--- lib/hammer_cli_foreman/api.rb
+++ lib/hammer_cli_foreman/api.rb
@@ -3,6 +3,7 @@ require 'hammer_cli_foreman/api/connection'
 module HammerCLIForeman
   module Api
     class UnauthorizedError < RuntimeError; end
+    class SessionExpired < UnauthorizedError; end
   end
 end
 
