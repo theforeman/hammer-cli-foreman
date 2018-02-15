47a4cb62f9c2de9e920cb412a5f30c83a4b62045
Martin Bačovský <martin.bacovsky@gmail.com>
2018-02-15 23:53:10 +0100

    Fixed the expectations in the tests

--- test/unit/option_sources/id_params_test.rb
+++ test/unit/option_sources/id_params_test.rb
@@ -11,17 +11,20 @@ describe HammerCLIForeman::OptionSources::IdParams do
     let(:id_params_source) { HammerCLIForeman::OptionSources::IdParams.new(hg_cmd) }
 
     it "skips param when set" do
-      hg_cmd.stubs(:get_resource_id).returns(1)
+      hg_cmd.expects(:get_resource_id).with { |res| res.name != :domains }.at_least(0).returns(nil)
+      hg_cmd.expects(:get_resource_id).with { |res| res.name == :domains }.never.returns(1)
       option_data = { 'option_domain_id' => 3, 'option_domain_name' => 'test' }
       params = id_params_source.get_options([], option_data)
       params.must_equal option_data
     end
 
     it "resolves param when unset" do
-      hg_cmd.stubs(:get_resource_id).returns(1)
+      hg_cmd.expects(:get_resource_id).with { |res| res.name != :domains }.at_least(0).returns(nil)
+      hg_cmd.expects(:get_resource_id).with { |res| res.name == :domains }.returns(1)
       option_data = { 'option_domain_id' => nil, 'option_domain_name' => 'test' }
+      expected_data = { 'option_domain_id' => 1, 'option_domain_name' => 'test' }
       params = id_params_source.get_options([], option_data)
-      params['option_domain_id'].must_equal 1
+      params.must_equal expected_data
     end
   end
 end
--- test/unit/option_sources/ids_params_test.rb
+++ test/unit/option_sources/ids_params_test.rb
@@ -11,17 +11,20 @@ describe HammerCLIForeman::OptionSources::IdsParams do
     let(:ids_params_source) { HammerCLIForeman::OptionSources::IdsParams.new(cmd) }
 
     it "skips param when set" do
-      cmd.stubs(:get_resource_ids).returns([1])
+      cmd.expects(:get_resource_ids).with { |res| res.name != :locations }.returns(nil)
+      cmd.expects(:get_resource_ids).with { |res| res.name == :locations }.never.returns([1])
       option_data = { 'option_location_ids' => [3], 'option_location_names' => 'test' }
       params = ids_params_source.get_options([], option_data)
       params.must_equal option_data
     end
 
     it "resolves param when unset" do
-      cmd.stubs(:get_resource_ids).returns([1])
-      option_data = { 'option_location_id' => nil, 'option_location_names' => 'test' }
+      cmd.expects(:get_resource_ids).with { |res| res.name != :locations }.returns(nil)
+      cmd.expects(:get_resource_ids).with { |res| res.name == :locations }.returns([1])
+      option_data = { 'option_location_ids' => nil, 'option_location_names' => 'test' }
+      expected_data = { 'option_location_ids' => [1], 'option_location_names' => 'test' }
       params = ids_params_source.get_options([], option_data)
-      params['option_location_ids'].must_equal [1]
+      params.must_equal expected_data
     end
   end
 end
