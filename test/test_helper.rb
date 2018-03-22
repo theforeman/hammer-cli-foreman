4409b7e4178a9ffbd71d188c8ba3f0b10bb17227
Oleh Fedorenko <ofedoren@redhat.com>
2018-03-22 13:21:08 +0000

    Fixes #22989 - Update to API doc form 1.17

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,7 @@ require "mocha/setup"
 require 'hammer_cli'
 require 'hammer_cli_foreman/testing/api_expectations'
 
-FOREMAN_VERSION = Gem::Version.new(ENV['TEST_API_VERSION'] || '1.15')
+FOREMAN_VERSION = Gem::Version.new(ENV['TEST_API_VERSION'] || '1.17')
 
 include HammerCLIForeman::Testing::APIExpectations
 HammerCLI.context[:api_connection].create('foreman') do
