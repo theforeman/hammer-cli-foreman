0d5ab8c7f0d534151b2f1877911e635059444c12
Dominik Hlavac <xhlava42@stud.fit.vutbr.cz>
2018-04-13 10:42:49 +0200

    Fixes #19651 - Listing all auth sources

--- lib/hammer_cli_foreman/auth_source.rb
+++ lib/hammer_cli_foreman/auth_source.rb
@@ -3,8 +3,22 @@ require 'hammer_cli_foreman/auth_source_ldap'
 module HammerCLIForeman
 
   class AuthSource < HammerCLIForeman::Command
-
+    resource :auth_sources
     subcommand 'ldap', HammerCLIForeman::AuthSourceLdap.desc, HammerCLIForeman::AuthSourceLdap
+
+    class ListCommand < HammerCLIForeman::ListCommand
+      desc _('List all auth sources.')
+
+      output do
+        field :id, _('Id')
+        field :name, _('Name')
+        field :type, _('Type of auth source')
+      end
+
+      build_options
+    end
+
+    autoload_subcommands
   end
 
 end
