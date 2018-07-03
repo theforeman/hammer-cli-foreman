7bff2274820eed3786dc437b0c9d84a73e31485f
Aditi Puntambekar <apuntamb@redhat.com>
2018-07-03 17:52:20 +0530

    Fixes #20360 - Setting list should show full names

---
+++ test/unit/settings_test.rb
@@ -0,0 +1,45 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+require File.join(File.dirname(__FILE__), 'apipie_resource_mock')
+
+require 'hammer_cli_foreman/settings'
+
+describe HammerCLIForeman::Settings do
+
+  include CommandTestHelper
+
+  context "ListCommand" do
+
+    before do
+      ResourceMocks.mock_action_call(:settings, :index, [])
+    end
+
+    let(:cmd) { HammerCLIForeman::Settings::ListCommand.new("", ctx) }
+
+    context "parameters" do
+      it_should_accept "no arguments"
+      it_should_accept_search_params
+    end
+
+    context "output" do
+      let(:expected_record_count) { count_records(cmd.resource.call(:index)) }
+
+      it_should_print_n_records
+      it_should_print_column "Name"
+      it_should_print_column "Full name"
+      it_should_print_column "Value"
+      it_should_print_column "Description"
+    end
+
+  end
+
+  context "UpdateCommand" do
+    let(:cmd) { HammerCLIForeman::Settings::UpdateCommand.new("", ctx) }
+
+    context "parameters" do
+      it_should_accept "name", ["--name=setting1", "--value=setting2"]
+      it_should_accept "id", ["--id=1", "--value=setting2"]
+    end
+
+  end
+end
+
