04b0b8ffd7a7234ef6f0ebb02252c8d18c3c2e77
Chris Roberts <chrobert@redhat.com>
2018-10-05 14:50:32 -0400

    Fixes #25192 - Update VMware helpers and host create page

--- lib/hammer_cli_foreman/compute_resources/vmware/host_help_extenstion.rb
+++ lib/hammer_cli_foreman/compute_resources/vmware/host_help_extenstion.rb
@@ -3,45 +3,49 @@ module HammerCLIForeman
     module VMware
       class HostHelpExtenstion
         INTERFACE_TYPES = %w(
-          VirtualVmxnet,
-          VirtualVmxnet2,
           VirtualVmxnet3,
-          VirtualE1000,
-          VirtualE1000e,
-          VirtualPCNet32
+          VirtualE1000
         )
 
         def name
-          _('VMWare')
+          _('VMware')
         end
 
         def host_create_help(h)
           h.section '--compute-attributes' do |h|
             h.list([
-              ['cpus',                 _("Cpu count")],
-              ['corespersocket',       _("Number of cores per socket (applicable to hardware versions < 10 only)")],
-              ['memory_mb',            _("Integer number, amount of memory in MB")],
-              ['cluster',              _("Cluster id from VMware")],
-              ['path',                 _("Path to folder")],
-              ['guest_id',             _("Guest OS id form VMware")],
-              ['scsi_controller_type', _("Id of the controller from VMware")],
-              ['hardware_version',     _("Hardware version id from VMware")],
-              ['start',                _("Must be a 1 or 0, whether to start the machine or not")]
+              ['cpus',                 _('CPU count')],
+              ['corespersocket',       _('Number of cores per socket (applicable to hardware versions < 10 only)')],
+              ['memory_mb',            _('Integer number, amount of memory in MB')],
+              ['firmware',             'automatic/bios/efi'],
+              ['cluster',              _('Cluster ID from VMware')],
+              ['resource_pool',        _('Resource Pool ID from VMware')],
+              ['path',                 _('Path to folder')],
+              ['guest_id',             _('Guest OS ID form VMware')],
+              ['scsi_controller_type', _('ID of the controller from VMware')],
+              ['hardware_version',     _('Hardware version ID from VMware')],
+              ['add_cdrom',            _('Must be a 1 or 0, Add a CD-ROM drive to the virtual machine')],
+              ['cpuHotAddEnabled',     _('Must be a 1 or 0, lets you add memory resources while the machine is on')],
+              ['memoryHotAddEnabled',  _('Must be a 1 or 0, lets you add CPU resources while the machine is on')],
+              ['start',                _("Must be a 1 or 0, whether to start the machine or not")],
+              ['annotation',           _("Annotation Notes")]
             ])
           end
           h.section '--interface' do |h|
             h.list([
               ['compute_type', interface_type_description(h)],
-              ['compute_network', _('Network id from VMware')]
+              ['compute_network', _('Network ID from VMware')]
             ])
           end
           h.section '--volume' do |h|
             h.list([
               ['name'],
-              ['datastore',  _('Datastore id from VMware')],
-              ['size_gb',    _('Integer number, volume size in GB')],
-              ['thin',       'true/false'],
-              ['eager_zero', 'true/false']
+              ['storage_pod', _('Storage Pod ID from VMware')],
+              ['datastore',   _('Datastore ID from VMware')],
+              ['size_gb',     _('Integer number, volume size in GB')],
+              ['thin',        'true/false'],
+              ['eager_zero',  'true/false'],
+              ['mode',        'persistent/independent_persistent/independent_nonpersistent']
             ])
           end
         end
@@ -53,7 +57,7 @@ module HammerCLIForeman
             _('Type of the network adapter, for example one of:'),
             h.indent(INTERFACE_TYPES),
             _("See documentation center for your version of vSphere to find more details about available adapter types:"),
-            h.indent("https://www.vmware.com/support/pubs/")
+            h.indent('https://www.vmware.com/support/pubs/')
           ].join("\n")
         end
       end
