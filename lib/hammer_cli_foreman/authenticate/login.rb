3c7f8916835b8fd3dcc19516afa342e6e9ff5130
Martin Bačovský <martin.bacovsky@gmail.com>
2019-11-20 16:58:28 +0100

    Fixes #28318 - Allow login in tty-less execution

--- lib/hammer_cli_foreman/authenticate/login.rb
+++ lib/hammer_cli_foreman/authenticate/login.rb
@@ -4,17 +4,12 @@ module HammerCLIForeman
       def execute_with_params(auth_type, *args)
         connection = HammerCLIForeman.foreman_api_reconnect(auth_type)
         if !(connection.authenticator.is_a?(HammerCLIForeman::Api::SessionAuthenticatorWrapper))
-          HammerCLI.interactive_output.say(_("Can't perform login. Make sure sessions are enabled in hammer"\
-            " configuration file."))
-          return HammerCLI::EX_USAGE
+          raise ArgumentError, _("Can't perform login. Make sure sessions are enabled in hammer"\
+            " configuration file.")
         end
         connection.authenticator.set_auth_params(*args)
         connection.authenticator.force_user_change
         connection.login
-
-        HammerCLI.interactive_output.say(_("Successfully logged in as '%s'.") %
-          connection.authenticator.user)
-        HammerCLI::EX_OK
       end
     end
   end
