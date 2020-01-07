c70052c12b427191d63e86348c1b089ea806738a
James Mead <github@floehopper.org>
2020-01-07 10:23:11 +0000

    Fixes #28420: Unpin mocha version & fix deprecation warning (#476)
    The regression introduced in mocha v1.10.0 which led to its version
    being pinned to v1.9.0 was fixed in mocha v1.11.2 which is currently the
    latest version, so it is nowsafe to unpin the version.
    
    Requiring 'mocha/setup' was deprecated in v1.10.0, so I've also fixed
    that with the change to `test/test_helper.rb`.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,7 @@ SimpleCov.root Pathname.new(File.dirname(__FILE__) + "../../../")
 require 'minitest/autorun'
 require 'minitest/spec'
 require "minitest-spec-context"
-require "mocha/setup"
+require "mocha/minitest"
 
 require 'hammer_cli'
 require 'hammer_cli_foreman/testing/api_expectations'
