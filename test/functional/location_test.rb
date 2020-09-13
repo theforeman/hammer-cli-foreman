93d2f6308b8ae22f28b00f79c29eee12f8c57a8b
yifatmakias <ymakias@redhat.com>
2020-09-13 17:13:34 +0300

    Fixes #30829 - added missing tests to location test

--- test/functional/location_test.rb
+++ test/functional/location_test.rb
@@ -139,3 +139,137 @@ describe "parameters" do
     end
   end
 end
+
+describe 'create' do
+  before do
+    @cmd = %w(location create)
+  end
+
+  it 'should print error missing argument name' do
+    expected_result = "Could not create the location:\n  Missing arguments for '--name'.\n"
+
+    api_expects(:locations, :index)
+
+    result = run_cmd(@cmd)
+    assert_match(expected_result, result.err)
+  end
+
+  it 'should create a location' do
+    params = ['--name=test-location']
+
+    api_expects(:locations, :index)
+    api_expects(:locations, :create, 'Create a locations') do |params|
+      (params['location']['name'] == 'test-location')
+    end
+
+    result = run_cmd(@cmd + params)
+    assert_cmd(success_result("Location created.\n"), result)
+  end
+end
+
+describe 'delete' do
+  before do
+    @cmd = %w(location delete)
+  end
+
+  it 'should print error missing argument id' do
+    expected_result = "Could not delete the location:\n  Missing arguments for '--id'.\n"
+
+    api_expects(:locations, :index)
+
+    result = run_cmd(@cmd)
+    assert_match(expected_result, result.err)
+  end
+
+  it 'should delete a location' do
+    params = ['--id=1']
+
+    api_expects(:locations, :index)
+    api_expects(:locations, :destroy, 'Delete a location').with_params(id: '1')
+
+    result = run_cmd(@cmd + params)
+    assert_cmd(success_result("Location deleted.\n"), result)
+  end
+end
+
+describe 'info' do
+  before do
+    @cmd = ['location', 'info']
+    @location = {
+        id: 1,
+        title: 'Default Location',
+        name: 'Default Location',
+        users: [],
+        smart_proxies: [],
+        subnets: [],
+        compute_resources: [],
+        media: [],
+        ptables: [],
+        provisioning_templates: [],
+        domains: [],
+        realms: [],
+        environments: [],
+        hostgroups: [],
+        organizations: [],
+        parameters: []
+    }
+  end
+
+  it 'should return the info of a location' do
+    params = ['--id', 1]
+    api_expects(:locations, :index)
+    api_expects(:locations, :show, 'Info location').returns(@location)
+
+    output = OutputMatcher.new([
+                                 'Id:                 1',
+                                 'Title:              Default Location',
+                                 'Name:               Default Location',
+                                 'Users:',
+                                 '',
+                                 'Smart proxies:',
+                                 '',
+                                 'Subnets:',
+                                 '',
+                                 'Compute resources:',
+                                 '',
+                                 'Installation media:',
+                                 '',
+                                 'Templates:',
+                                 '',
+                                 'Partition Tables:',
+                                 '',
+                                 'Domains:',
+                                 '',
+                                 'Realms:',
+                                 '',
+                                 'Environments:',
+                                 '',
+                                 'Hostgroups:',
+                                 '',
+                                 'Parameters:',
+    ])
+
+    expected_result = success_result(output)
+    result = run_cmd(@cmd + params)
+    assert_cmd(expected_result, result)
+  end
+end
+
+describe 'update' do
+  before do
+    @cmd = ['location', 'update']
+  end
+
+  it 'should update a location' do
+    params = ['--id=1', '--new-name=Default Location test']
+
+    api_expects(:locations, :index)
+    api_expects(:locations, :update, 'Update a location') do |params|
+      (params['location']['id'] == '1')
+      (params['location']['name'] == 'Default Location test')
+    end
+
+    result = run_cmd(@cmd + params)
+    assert_cmd(success_result("Location updated.\n"), result)
+  end
+end
