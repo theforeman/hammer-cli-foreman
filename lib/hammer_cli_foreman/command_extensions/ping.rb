6f61e86de7d026263fc35a6b588ccf7c8426ec37
Oleh Fedorenko <ofedoren@redhat.com>
2020-09-29 10:09:42 +0000

    Fixes #30496 - Return non-zero exit code if services failed

--- lib/hammer_cli_foreman/command_extensions/ping.rb
+++ lib/hammer_cli_foreman/command_extensions/ping.rb
@@ -6,8 +6,13 @@ module HammerCLIForeman
           status = data['results']['foreman']['database']['active']
           data['results']['foreman']['database']['active'] = status ? 'ok' : 'FAIL'
           duration = data['results']['foreman']['database']['duration_ms']
-          data['results']['foreman']['database']['duration_ms'] = _("Duration: %sms") % duration
+          data['results']['foreman']['database']['duration_ms'] = _('Duration: %sms') % duration
         end
+      end
+
+      def self.failed?(services)
+        services.each_value.any? { |s| s['status'] == _('FAIL') } ||
+          services['foreman']['database']['active'] == 'FAIL'
       end
     end
   end
--- lib/hammer_cli_foreman/ping.rb
+++ lib/hammer_cli_foreman/ping.rb
@@ -1,22 +1,31 @@
 module HammerCLIForeman
   class PingCommand < HammerCLIForeman::Command
-      resource :ping
+    resource :ping
 
-      class ForemanCommand < HammerCLIForeman::Command
-        action :ping
-        command_name 'foreman'
+    class ForemanCommand < HammerCLIForeman::Command
+      action :ping
+      command_name 'foreman'
 
-        output do
-          from 'foreman' do
-            field :database, _('database'), Fields::Label do
-              field :active, _('Status')
-              field :duration_ms, _('Server Response')
-            end
+      output do
+        from 'foreman' do
+          field :database, _('database'), Fields::Label do
+            field :active, _('Status')
+            field :duration_ms, _('Server Response')
           end
         end
-
-        extend_with(HammerCLIForeman::CommandExtensions::Ping.new)
       end
+
+      def execute
+        response = send_request
+        print_data(response)
+
+        return 1 if HammerCLIForeman::CommandExtensions::Ping.failed?(response)
+
+        HammerCLI::EX_OK
+      end
+
+      extend_with(HammerCLIForeman::CommandExtensions::Ping.new)
+    end
 
     self.default_subcommand = 'foreman'
     autoload_subcommands
