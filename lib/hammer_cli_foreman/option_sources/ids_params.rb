21f3a52d5ba529c881ce719edba247607c19cc6e
Oleh Fedorenko <ofedoren@redhat.com>
2021-05-13 13:21:39 +0000

    Fixes #32392 - Force name resolving due defaults

--- lib/hammer_cli_foreman/option_sources/ids_params.rb
+++ lib/hammer_cli_foreman/option_sources/ids_params.rb
@@ -13,15 +13,34 @@ module HammerCLIForeman
         IdArrayParamsFilter.new(:only_required => false).for_action(@command.resource.action(@command.action))
       end
 
-      def get_options(defined_options, result)
-        return result if @command.action.nil?
+      def needs_resolving?(param_option, param_resource, all_opts)
+        return false unless param_updatable?(param_resource)
+
+        searchables_set = @command.searchables.for(param_resource).any? do |s|
+          option = HammerCLI.option_accessor_name("#{param_resource.singular_name}_#{s.plural_name}")
+          !all_opts[option].nil?
+        end
+        return all_opts[param_option].nil? unless searchables_set
+
+        # Remove set '<resource_name>_ids' option to force resolving in case of
+        # '<resource_name>_[names|titles]' was set
+        all_opts.delete(param_option)
+        true
+      end
+
+      def get_options(_defined_options, result)
         # resolve all '<resource_name>_ids' parameters if they are defined as options
+        return result if @command.action.nil?
+
         available_ids_params.each do |api_param|
           param_resource = HammerCLIForeman.param_to_resource(api_param.name)
-          if result[HammerCLI.option_accessor_name(api_param.name)].nil? && param_updatable?(param_resource)
-            resource_ids = @command.get_resource_ids(param_resource, :scoped => true, :required => api_param.required?, :all_options => result)
-            result[HammerCLI.option_accessor_name(api_param.name)] = resource_ids if resource_ids
-          end
+          param_option = HammerCLI.option_accessor_name(api_param.name)
+          next unless needs_resolving?(param_option, param_resource, result)
+
+          resource_ids = @command.get_resource_ids(
+            param_resource, scoped: true, required: api_param.required?, all_options: result
+          )
+          result[param_option] = resource_ids if resource_ids
         end
         result
       end
