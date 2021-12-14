f66fdfe1f402dd600a91da8d047b9c9576faa3b5
aabramov <aabramov@redhat.com>
2021-12-14 10:36:31 +0200

    Fixes #33130 - Fix to consistent message whem arguments are missing

--- lib/hammer_cli_foreman/command_extensions/update_common.rb
+++ lib/hammer_cli_foreman/command_extensions/update_common.rb
@@ -7,7 +7,12 @@ module HammerCLIForeman
 
       request_params do |params, command_object|
         update_params = params[command_object.resource.singular_name]
-        command_object.context[:action_message] = :nothing_to_do if update_params && update_params.empty?
+        update_params =  update_params.nil? ? params : update_params.values.select { | val| !val.nil?}
+        command_object.context[:action_message] = :nothing_to_do unless valid?(update_params)
+      end
+
+      def self.valid?(params)
+        params.nil? ? false : !(params.empty? || params.select { | param | param.is_a?(Hash) && param.empty? }.count == params.count)
       end
     end
   end
