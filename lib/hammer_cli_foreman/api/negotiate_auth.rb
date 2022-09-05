2f23784d6aaf2d7de3455cd4c6a48df81673c87e
Oleh Fedorenko <ofedoren@redhat.com>
2022-09-05 16:23:53 +0000

    Refs #35473 - Change auth endpoint for negotiation

--- lib/hammer_cli_foreman/api/negotiate_auth.rb
+++ lib/hammer_cli_foreman/api/negotiate_auth.rb
@@ -2,7 +2,7 @@ module HammerCLIForeman
   module Api
     class NegotiateAuth < ApipieBindings::Authenticators::Negotiate
       def initialize(foreman_url, **options)
-        super("#{foreman_url}/users/extlogin", HammerCLI::SSLOptions.new.get_options(foreman_url).merge(options))
+        super("#{foreman_url}/api/users/extlogin", HammerCLI::SSLOptions.new.get_options(foreman_url).merge(options))
       end
 
       def user
