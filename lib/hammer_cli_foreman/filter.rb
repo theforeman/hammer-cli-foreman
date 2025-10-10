ca8536d9adfe226a6fbbf157a2e548a7ad74e29b
Adam Ruzicka <aruzicka@redhat.com>
2025-10-10 11:26:29 +0200

    Fixes #38805 - Remove options related to filter overrides

--- lib/hammer_cli_foreman/filter.rb
+++ lib/hammer_cli_foreman/filter.rb
@@ -9,8 +9,6 @@ module HammerCLIForeman
         field :id, _("Id")
         field :resource_type, _("Resource type")
         field :search, _("Search")
-        field :unlimited?, _("Unlimited?"), Fields::Boolean
-        field :override?, _("Override?"), Fields::Boolean
         field :role, _("Role"), Fields::Reference
         field :permissions, _("Permissions"), Fields::List
       end
@@ -27,8 +25,15 @@ module HammerCLIForeman
 
     class InfoCommand < HammerCLIForeman::InfoCommand
       output ListCommand.output_definition do
-        HammerCLIForeman::References.taxonomies(self)
         HammerCLIForeman::References.timestamps(self)
+        from :role do
+          collection :locations, _("Locations"), numbered: false, hide_blank: true do
+            custom_field Fields::Reference, name_key: :title
+          end
+          collection :organizations, _("Organizations"), numbered: false, hide_blank: true do
+            custom_field Fields::Reference, name_key: :title
+          end
+        end
       end
 
       def extend_data(filter)
@@ -42,80 +47,17 @@ module HammerCLIForeman
     end
 
 
-    module TaxonomyCheck
-
-      def self.included(base)
-        def taxonomy_options?
-          opt_names = ['location_ids', 'organization_ids']
-          opt_names += resolver.searchables(:locations).map { |s| 'location_' + s.plural_name }
-          opt_names += resolver.searchables(:organizations).map { |s| 'organization_' + s.plural_name }
-          opt_names.any? { |opt| send(HammerCLI.option_accessor_name(opt)) }
-        end
-
-        def signal_override_usage_error
-          signal_usage_error _('Organizations and locations can be set only for overriding filters.')
-        end
-
-        base.extend_help do |base_h|
-          base_h.section(_('Overriding organizations and locations')) do |section_h|
-            override_condition = "--override=true"
-            org_opts = '--organization[s|-ids]'
-            loc_opts = '--location[s|-ids]'
-
-            section_h.text(_("Filters inherit organizations and locations from its role by default. This behavior can be changed by setting %{condition}.%{wsp}" +
-              "Therefore options %{org_opts} and %{loc_opts} are applicable only when the override flag is set.") % {
-              :wsp => "\n",
-              :org_opts => org_opts,
-              :loc_opts => loc_opts,
-              :condition => override_condition
-            })
-          end
-        end
-      end
-    end
-
-
     class CreateCommand < HammerCLIForeman::CreateCommand
-      include TaxonomyCheck
-
       success_message _("Permission filter for [%<resource_type_label>s] created.")
       failure_message _("Could not create the permission filter")
-
-      def validate_options
-        signal_override_usage_error if !option_override && taxonomy_options?
-      end
 
       build_options
     end
 
 
     class UpdateCommand < HammerCLIForeman::UpdateCommand
-      include TaxonomyCheck
-
       success_message _("Permission filter for [%<resource_type_label>s] updated.")
       failure_message _("Could not update the permission filter")
-
-      def request_params
-        params = super
-        unless override?
-          # Clear taxonomies in case the filter is switching override from true to false
-          params['filter']['location_ids'] = [] if !@filter || !@filter['locations'].empty?
-          params['filter']['organization_ids'] = [] if !@filter || !@filter['organizations'].empty?
-        end
-        params
-      end
-
-      def validate_options
-        signal_override_usage_error if !override? && taxonomy_options?
-      end
-
-      def override?
-        if option_override.nil?
-          filter['override?']
-        else
-          option_override
-        end
-      end
 
       def filter
         @filter ||= HammerCLIForeman.foreman_resource!(:filters).action(:show).call({ :id => get_identifier }, request_headers, request_options)
